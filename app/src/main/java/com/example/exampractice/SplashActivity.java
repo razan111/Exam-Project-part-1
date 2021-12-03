39f30af04b8fa8cbc4a2b70f52c6037c8105ce24
razan111 <razan15-12966@diu.edu.bd>
2021-12-03 23:27:45 +0600

    Part-1

---
+++ app/src/main/java/com/example/exampractice/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.example.exampractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    private TextView appName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        appName = findViewById(R.id.app_name);
+
+
+
+//        Animation Show
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.myanim);
+        appName.setAnimation(anim);
+
+        new Thread() {
+            @Override
+            public void run(){
+
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+
+        }.start();
+
+
+    }
+}
